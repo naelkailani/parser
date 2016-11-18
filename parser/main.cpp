5c0f3be659c9d96df08d3699a59a898bc8c9c808
Nael Kilani <naelkilani@Naels-MacBook-Pro.local>
2016-11-18 18:24:39 +0200

    Initial Commit

---
+++ parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  parser
+//
+//  Created by Nael Kilani on 11/18/16.
+//  Copyright © 2016 Nael Kilani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
